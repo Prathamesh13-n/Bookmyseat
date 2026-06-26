337c052c98d3836731acbf967c8d9c2bf8ed1044
Prathamesh Nehete <prathameshnehete13@gmail.com>
2026-06-26 06:55:04 +0530

    Added remaining features

--- bookmyseat/urls.py
+++ bookmyseat/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('',include('users.urls')),
     path('movies/', include('movies.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
